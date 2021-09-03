bd9cdbe84e1b615e65e90e4b72e810cca60d7127
Shuvodip Ray <shuvodip@pphackathonteam5.onmicrosoft.com>
2021-09-03 21:20:05 +0530

    changed default routing rule to bind the student action method on page load.

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ASP.NET_MVC_Exercise
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Student", id = UrlParameter.Optional }
             );
         }
     }
