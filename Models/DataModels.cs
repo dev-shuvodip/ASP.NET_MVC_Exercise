08cd9683736cc0672bb0c1ab8d263a20f9c9c97e
Shuvodip Ray <shuvodip@pphackathonteam5.onmicrosoft.com>
2021-09-03 21:21:58 +0530

    changed data model

--- Models/DataModels.cs
+++ Models/DataModels.cs
@@ -7,10 +7,21 @@ namespace ASP.NET_MVC_Exercise.Models
 {
     public class Student
     {
-        public int ID { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
         public string Qualification { get; set; }
         public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public string CustomID { get; set; }
+    }
+
+    public class CustomerData
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+        public long CustomerContact { get; set; }
+
     }
 }
